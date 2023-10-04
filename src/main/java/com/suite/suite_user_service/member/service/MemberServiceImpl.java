ea33a1cbab73463be20b1925513917c3b5c3ddbe
김대현 <zxz4641@gmail.com>
2023-10-04 18:12:55 +0900

    fix: sen SMS fix

--- src/main/java/com/suite/suite_user_service/member/service/MemberServiceImpl.java
+++ src/main/java/com/suite/suite_user_service/member/service/MemberServiceImpl.java
@@ -143,7 +143,7 @@ public class MemberServiceImpl implements MemberService {
     public String sendSms(String phoneNumber) {
         String authCode = generateRandomNumber();
         try {
-
+            System.out.println("phone : " + phoneNumber);
             PublishResponse response = snsClient.publish(PublishRequest.builder()
                     .phoneNumber(phoneNumber)
                     .message("[SUITE] 본인 확인을 위해 인증번호 [" + authCode + "]를 입력해주세요.")
