a53a45c72db12d06c0f64e54cc495b771eed57b2
반영환 <lopahn2@gmail.com>
2023-10-22 01:12:16 +0900

    feat: entityScan append

--- src/main/java/com/suite/suite_user_service/SuiteUserServiceApplication.java
+++ src/main/java/com/suite/suite_user_service/SuiteUserServiceApplication.java
@@ -2,6 +2,7 @@ package com.suite.suite_user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
                 org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
         }
 )
+@EntityScan("com.suite.suite_user_service.member.entity")
 public class SuiteUserServiceApplication {
 
     public static void main(String[] args) {
