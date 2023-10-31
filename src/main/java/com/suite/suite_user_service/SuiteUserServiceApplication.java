f4b4df7acd072d717ba68366c7ae3af4ac514510
김대현 <zxz4641@gmail.com>
2023-10-31 12:24:13 +0900

    fix: stmp gmail

--- src/main/java/com/suite/suite_user_service/SuiteUserServiceApplication.java
+++ src/main/java/com/suite/suite_user_service/SuiteUserServiceApplication.java
@@ -17,7 +17,7 @@ public class SuiteUserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SuiteUserServiceApplication.class, args);
     }
-
+//
 
 
 }
